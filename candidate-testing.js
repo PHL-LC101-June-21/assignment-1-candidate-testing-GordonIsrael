d9971d25eecbf4a66116a985da0e5ac3d4b72135
IsraelGordon <85712508+GordonIsrael@users.noreply.github.com>
2021-06-28 03:37:19 +0000

    tried to input question and answers, but am having difficulties

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = ("");
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = ("Who was the first American in space? ");
+let correctAnswer = ("Sally Ride");
+let candidateAnswer = ("");
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,12 +15,15 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+let candidateName = input.question("What is your name: ")
+console.log("Welcome aboard, " + candidateName + "!");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+console.log(question); 
+if (question = correctAnswer) {
+  console.log("This is Right!")}
 
 }
 
